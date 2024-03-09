6fc8f5619c538e96ba9a834ef7e2c1a80dc8f910
Alejandro Beltre <1115242@est.intec.edu.do>
2024-03-09 11:22:25 -0400

    Added folders for all the screens designed and the home screen for the user screen

---
+++ src/app/usuarios/home/index.tsx
@@ -0,0 +1,298 @@
+import type { NextPage } from "next";
+import styles from "./index.module.css";
+
+const USUARIOS: NextPage = () => {
+  return (
+    <div className={styles.usuarios}>
+      <div className={styles.group111Wrapper}>
+        <img
+          className={styles.group111}
+          loading="lazy"
+          alt=""
+          src="/group-11-1@2x.png"
+        />
+      </div>
+      <main className={styles.lineParent}>
+        <div className={styles.frameChild} />
+        <section className={styles.frameParent}>
+          <div className={styles.menuParent}>
+            <div className={styles.menu}>
+              <div className={styles.men}>
+                <div className={styles.registrosWrapper}>
+                  <h1 className={styles.registros}>Registros</h1>
+                </div>
+                <div className={styles.depth3Frame0}>
+                  <div className={styles.depth4Frame0}>
+                    <div className={styles.depth5Frame0}>
+                      <div className={styles.depth6Frame1}>
+                        <div className={styles.depth7Frame0}>
+                          <div className={styles.image4} />
+                          <input
+                            className={styles.nuevoUsuarioText}
+                            placeholder="Usuarios"
+                            type="text"
+                          />
+                        </div>
+                        <div className={styles.depth7Frame1}>
+                          <div className={styles.depth8Frame0}>
+                            <div className={styles.depth9Frame0}>
+                              <img
+                                className={styles.vector0}
+                                loading="lazy"
+                                alt=""
+                                src="/vector--0.svg"
+                              />
+                              <div className={styles.depth10Frame0} />
+                            </div>
+                          </div>
+                          <div className={styles.depth8Frame1}>
+                            <div className={styles.depth9Frame01}>
+                              <div className={styles.perfiles}>Perfiles</div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className={styles.depth7Frame2}>
+                          <div className={styles.depth8Frame01}>
+                            <div className={styles.depth9Frame02}>
+                              <img
+                                className={styles.vector01}
+                                loading="lazy"
+                                alt=""
+                                src="/vector--0-1.svg"
+                              />
+                              <div className={styles.depth10Frame01} />
+                            </div>
+                          </div>
+                          <div className={styles.depth8Frame11}>
+                            <div className={styles.depth9Frame03}>
+                              <div className={styles.productos}>Productos</div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className={styles.depth7Frame3}>
+                          <div className={styles.depth8Frame02}>
+                            <div className={styles.depth9Frame04}>
+                              <img
+                                className={styles.vector02}
+                                loading="lazy"
+                                alt=""
+                                src="/vector--0-2.svg"
+                              />
+                              <div className={styles.depth10Frame02} />
+                            </div>
+                          </div>
+                          <div className={styles.depth8Frame12}>
+                            <div className={styles.depth9Frame05}>
+                              <div className={styles.clientes}>Clientes</div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className={styles.depth7Frame4}>
+                          <div className={styles.depth8Frame03}>
+                            <div className={styles.depth9Frame06}>
+                              <img
+                                className={styles.vector03}
+                                loading="lazy"
+                                alt=""
+                                src="/vector--0-3.svg"
+                              />
+                              <div className={styles.depth10Frame03} />
+                            </div>
+                          </div>
+                          <div className={styles.depth8Frame13}>
+                            <div className={styles.depth9Frame07}>
+                              <div className={styles.servicios}>Servicios</div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className={styles.depth7Frame5}>
+                          <div className={styles.depth8Frame04}>
+                            <div className={styles.depth9Frame08}>
+                              <img
+                                className={styles.vector04}
+                                loading="lazy"
+                                alt=""
+                                src="/vector--0-4.svg"
+                              />
+                              <div className={styles.depth10Frame04} />
+                            </div>
+                          </div>
+                          <div className={styles.depth8Frame14}>
+                            <div className={styles.depth9Frame09}>
+                              <div className={styles.cotizaciones}>
+                                Cotizaciones
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className={styles.depth7Frame6}>
+                          <div className={styles.depth8Frame05}>
+                            <div className={styles.depth9Frame010}>
+                              <img
+                                className={styles.vector05}
+                                loading="lazy"
+                                alt=""
+                                src="/vector--0-5.svg"
+                              />
+                              <div className={styles.depth10Frame05} />
+                            </div>
+                          </div>
+                          <div className={styles.depth8Frame15}>
+                            <div className={styles.depth9Frame011}>
+                              <div className={styles.facturas}>Facturas</div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className={styles.depth7Frame7}>
+                          <div className={styles.depth8Frame06}>
+                            <div className={styles.depth9Frame012}>
+                              <img
+                                className={styles.vector06}
+                                loading="lazy"
+                                alt=""
+                                src="/vector--0-5.svg"
+                              />
+                              <div className={styles.depth10Frame06} />
+                            </div>
+                          </div>
+                          <div className={styles.depth8Frame16}>
+                            <div className={styles.depth9Frame013}>
+                              <div className={styles.cuentasPorCobrar}>
+                                Cuentas por cobrar
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className={styles.depth5Frame1} />
+                  </div>
+                </div>
+              </div>
+              <img
+                className={styles.frameIcon}
+                loading="lazy"
+                alt=""
+                src="/vector--0-2.svg"
+              />
+            </div>
+            <div className={styles.frameWrapper}>
+              <button className={styles.nuevoUsuarioWrapper}>
+                <div className={styles.nuevoUsuario}>Nuevo Usuario</div>
+              </button>
+            </div>
+          </div>
+          <div className={styles.usuariosText}>
+            <div className={styles.frameGroup}>
+              <div className={styles.usuariosParent}>
+                <h1 className={styles.usuarios1}>Usuarios</h1>
+                <h3 className={styles.manejarUsuariosEn}>
+                  Manejar usuarios en la aplicación
+                </h3>
+              </div>
+              <div className={styles.depth5Frame01}>
+                <div className={styles.depth6Frame0}>
+                  <div className={styles.depth7Frame01}>
+                    <img
+                      className={styles.vector07}
+                      alt=""
+                      src="/vector--0-8.svg"
+                    />
+                    <div className={styles.depth8Frame07} />
+                  </div>
+                </div>
+                <input
+                  className={styles.depth6Frame11}
+                  placeholder="Buscar usuario"
+                  type="text"
+                />
+              </div>
+              <div className={styles.rectangleParent}>
+                <div className={styles.frameItem} />
+                <div className={styles.frameContainer}>
+                  <div className={styles.nombresParent}>
+                    <b className={styles.nombres}>Nombres</b>
+                    <div className={styles.bernardoBaez}>Bernardo Baez</div>
+                  </div>
+                  <div className={styles.alejandroBeltre}>Alejandro Beltre</div>
+                </div>
+                <div className={styles.alejandroBeltre1}>
+                  <div className={styles.emailAddressesParent}>
+                    <div className={styles.emailAddresses}>
+                      <b className={styles.correosElectrnicos}>
+                        Correos electrónicos
+                      </b>
+                      <a
+                        className={styles.bernardobaezgmailcom}
+                        href="mailto:bernardobaez@gmail.com"
+                        target="_blank"
+                      >
+                        bernardobaez@gmail.com
+                      </a>
+                    </div>
+                    <div className={styles.alejandrobeltegmailcom}>
+                      alejandrobelte@gmail.com
+                    </div>
+                  </div>
+                  <div className={styles.roles}>
+                    <div className={styles.usersAdmin}>
+                      <b className={styles.roles1}>Roles</b>
+                    </div>
+                    <div className={styles.editDelete}>
+                      <button className={styles.adminWrapper}>
+                        <div className={styles.admin}>Admin</div>
+                      </button>
+                      <button className={styles.usuarioWrapper}>
+                        <div className={styles.usuario}>Usuario</div>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.lastWeek}>
+                  <div className={styles.olderActivity}>
+                    <b className={styles.ltimaVezActivo}>Última vez activo</b>
+                    <b className={styles.acciones}>Acciones</b>
+                  </div>
+                  <div className={styles.actionsframe}>
+                    <div className={styles.hacesemana}>
+                      <div className={styles.hace1Semana}>Hace 1 semana</div>
+                    </div>
+                    <div className={styles.editframe}>
+                      <button className={styles.eliminarframe}>
+                        <div className={styles.editar}>Editar</div>
+                      </button>
+                      <button className={styles.eliminarframe1}>
+                        <div className={styles.eliminar}>Eliminar</div>
+                      </button>
+                    </div>
+                  </div>
+                  <div className={styles.lastvisited}>
+                    <div className={styles.line}>
+                      <div className={styles.hace1Ao}>Hace 1 año</div>
+                    </div>
+                    <div className={styles.correoselectrnicos}>
+                      <button className={styles.roles2}>
+                        <div className={styles.editar1}>Editar</div>
+                      </button>
+                      <div className={styles.eliminar1}>
+                        <button className={styles.nombres1}>
+                          <div className={styles.eliminar2}>Eliminar</div>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <div className={styles.alejandrobeltegmailcom1} />
+      <footer className={styles.negocia2024Wrapper}>
+        <div className={styles.negocia2024}>Negocia © 2024</div>
+      </footer>
+    </div>
+  );
+};
+
+export default USUARIOS;
