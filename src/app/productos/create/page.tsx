ad09fc0327f6f6023f6e2bfbaa16dc94aebe4ab7
bernardobaez <bernardbaez.beno@gmail.com>
2024-04-05 18:47:12 -0400

    ... no se que agregue

--- src/app/productos/create/page.tsx
+++ src/app/productos/create/page.tsx
@@ -25,7 +25,7 @@ interface form{
     product_name: string,
     stock: number,
     category_id: number,
-    price: number,
+    price: string
     File: string,
     url_image: string
 }
@@ -34,7 +34,15 @@ function ProductosCreate() {
     const setUrl = usePageStore((state) => state.changeUrl);
     const [products, setProducts] = useState<product[]>([]);
     const [category, setCategory] = useState<category[]>([]);
-    const [formData, setFormData] = useState<form>();
+    const [formData, setFormData] = useState({
+        product_name: "",
+        stock: 0,
+        category_id: 0,
+        price: 0,
+        File: "",
+        url_image: ""
+    });
+
     const [available, setAvailable] = useState(null);
     let inputFile = useRef(null);
 
@@ -66,8 +74,13 @@ function ProductosCreate() {
         }
     }
     
-    function handleChange(e: ChangeEvent<HTMLInputElement>){
-        // setFormData((prev)=> ({...prev, [e.target.name]: e.target.value}))
+    function handleChange(e: ChangeEvent<HTMLInputElement> | any){
+        const name = e.target.name;
+        const value = typeof(formData[name]) == "number" ? parseInt(e.target.value) : e.target.value;
+
+        setFormData((prev)=> ({...prev, [name]: value}))
+        
+        console.log({ ...formData, [e.target.name]: value });
     }
 
     function handleSubmit(e: FormEvent<HTMLFormElement>){
@@ -86,7 +99,7 @@ function ProductosCreate() {
         <form onSubmit={handleSubmit} className="p-5 register-smth min-h-[100%]">
             <div>
                 <h2 className="text-xl">Nombre del producto</h2>
-                <input type="text" name="product_name" id=""className={available == null ? " border-2 border-slate-500 outline-none" : available ? "available" : " border-2 border-red-500 outline-none"} onChange={(e)=>searchProduct(e)} required/>
+                <input type="text" name="product_name" id=""className={available == null ? " border-2 border-slate-500 outline-none" : available ? "available" : " border-2 border-red-500 outline-none"} onChange={(e)=>{ searchProduct(e); handleChange(e) }} required/>
                 
                 {available == null ? <h3 className="text-slate-500 flex items-center gap-x-2 mt-2">
                 Buscando nombre
@@ -102,15 +115,15 @@ function ProductosCreate() {
 
             <div>
                 <h2 className="text-xl">Cantidad en stock</h2>
-                <input type="number" name="stock" id="" min={1} defaultValue={1} required/>
+                <input type="number" name="stock" id="" min={1} defaultValue={1} onChange={(e)=> handleChange(e)} required/>
             </div>
 
             <div>
                 <h2 className="text-xl">Tipo de producto</h2>
-                <select name="category_id" id="" required>
+                <select name="category_id" id="" onChange={(e)=>handleChange(e)} required>
                     {category.length == 0 ?
                     <option>NO CATEGORIAS</option>
-                    :category.map(cate=> <option key={cate.ID} id={"" + cate.ID}>{cate.category_name}</option> )}
+                    :category.map(cate=> <option key={cate.ID} id={"" + cate.ID} value={cate.ID}>{cate.category_name}</option> )}
                 </select>
             </div>
 
@@ -122,7 +135,7 @@ function ProductosCreate() {
             <input type="text" value="" name="url_image" className="hidden"/>
 
             <div>
-                <input type="File" id="miInputDeArchivo" className="hidden" ref={inputFile} required/>
+                <input type="File" id="miInputDeArchivo" className="hidden" name="File" onChange={(e)=>handleChange(e)} ref={inputFile} required/>
                 <button type="submit" className="px-12 text-white rounded-lg py-2 bg-violet-500 font-bold flex gap-x-2 items-center justify-center w-[30em] hover:text-violet-500 hover:bg-white transition-all border-2 border-violet-500" onClick={()=>{
                     if(inputFile.current){
                         inputFile.current.click();
@@ -133,10 +146,10 @@ function ProductosCreate() {
             </div>
 
             <div className="w-[30em] flex gap-x-4">
-                <button className="flex-grow bg-blue-500 py-2 text-white rounded-lg flex items-center justify-center gap-x-2">
+                <button className="flex-grow bg-blue-500 py-2 text-white rounded-lg flex items-center justify-center gap-x-2 hover:text-blue-500 hover:bg-white border-2 border-blue-500">
                     <FontAwesomeIcon icon={faSave}/>
                     CREAR</button>
-                <button className="flex-grow bg-red-500 py-2 text-white rounded-lg flex items-center justify-center gap-x-2">
+                <button className="flex-grow bg-red-500 py-2 text-white rounded-lg flex items-center justify-center gap-x-2 hover:text-red-500 hover:bg-white border-2 border-red-500">
                     <FontAwesomeIcon icon={faCancel}/>
                     CANCELAR</button>
             </div>
